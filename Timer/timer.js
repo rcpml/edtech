e1ff4f636ce5302b85cda4cb8e3216791171aeec
rcpml <recepemul@gmail.com>
2018-08-14 13:12:33 +0300

    Timer 1.1

--- Timer/timer.js
+++ Timer/timer.js
@@ -1,19 +1,24 @@
-//Time display, +5 & -5 button 
+//Time display, +5 & -5 button
 const timerMinutes = document.getElementById("timerminutes");
 const timerSeconds = document.getElementById("timerseconds");
+const timerHours = document.getElementById("timerhours");
 const minusOne = document.getElementById("minusone");
 const plusOne = document.getElementById("plusone");
 const minusFive = document.getElementById("minusfive");
 const plusFive = document.getElementById("plusfive");
 
-
-
-//Set default count value to 5 seconds
+//Set default count value to 5 minutes
 let timerCoreStarter = 300;
 var timerCoreValue = new Date(timerCoreStarter * 1000);
+var hours = Math.floor((timerCoreValue % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((timerCoreValue % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((timerCoreValue % (1000 * 60)) / 1000);
 //Output the minutes and seconds. Hide the minute part if it is less than 1 minute
+if (hours < 10) {
+  timerHours.textContent = "0" + hours;
+} else {
+  timerHours.textContent = hours;
+}
 if (minutes < 10) {
   timerMinutes.textContent = "0" + minutes;
 } else {
@@ -27,9 +32,15 @@ if (seconds < 10) {
 //Converting to minutes and seconds
 function timeCalcPost() {
   let timerCoreValue = new Date(timerCoreStarter * 1000);
+  let hours = Math.floor((timerCoreValue % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   let minutes = Math.floor((timerCoreValue % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((timerCoreValue % (1000 * 60)) / 1000);
   //Output the minutes and seconds. Hide the minute part if it is less than 1 minute
+  if (hours < 10) {
+    timerHours.textContent = "0" + hours;
+  } else {
+    timerHours.textContent = hours;
+  }
   if (minutes < 10) {
     timerMinutes.textContent = "0" + minutes;
   } else {
@@ -42,9 +53,8 @@ function timeCalcPost() {
   }
 }
 
-
 //-1 minute with each click
-minusOne.addEventListener('click', function () {
+minusOne.addEventListener("click", function () {
   if (timerCoreStarter > 120) {
     timerCoreStarter -= 60;
     timeCalcPost();
@@ -55,14 +65,13 @@ minusOne.addEventListener('click', function () {
 });
 
 //+1 minute with each click
-plusOne.addEventListener('click', function () {
+plusOne.addEventListener("click", function () {
   timerCoreStarter += 60;
   timeCalcPost();
 });
 
-
 //-5 minutes with each click
-minusFive.addEventListener('click', function () {
+minusFive.addEventListener("click", function () {
   if (timerCoreStarter > 300) {
     timerCoreStarter -= 300;
     timeCalcPost();
@@ -73,11 +82,10 @@ minusFive.addEventListener('click', function () {
 });
 
 //+5 minutes with each click
-plusFive.addEventListener('click', function () {
+plusFive.addEventListener("click", function () {
   timerCoreStarter += 300;
   timeCalcPost();
 });
-
 
 //Counter function and stop when it reaches zero.
 function counter() {
@@ -85,7 +93,7 @@ function counter() {
   timeCalcPost();
   if (timerCoreStarter == 0) {
     clearInterval(counterX);
-    document.getElementById('notificationaudio').play();
+    document.getElementById("notificationaudio").play();
   }
 }
 
@@ -98,13 +106,11 @@ function initiateTimer() {
     counterX = setInterval(counter, 1000);
   }
 }
-document.getElementById('initiatetimer').addEventListener("click", initiateTimer);
 
-//Pause
+//Pause - not used currently
 function pauseTimer() {
   clearInterval(counterX);
 }
-document.getElementById('pausetimer').addEventListener("click", pauseTimer);
 
 //Reset
 function resetTimer() {
@@ -112,4 +118,60 @@ function resetTimer() {
   timerCoreStarter = 300;
   timeCalcPost();
 }
-document.getElementById('resettimer').addEventListener("click", resetTimer);
+document.getElementById("resettimer").addEventListener("click", resetTimer);
+
+//Start & pause the timer
+function start() {
+  if (timerCoreStarter > 0) {
+    clearInterval(counterX);
+    counterX = setInterval(counter, 1000);
+  }
+}
+document.getElementById("start").addEventListener("click", start);
+
+function pause() {
+  clearInterval(counterX);
+}
+document.getElementById("pause").addEventListener("click", pause);
+
+
+
+//Hide all except the timer when the counter starts
+function hideOthers() {
+  $(".clear").fadeOut(500);
+  $("#timerheading").addClass("timerheading");
+  event.stopPropagation();
+}
+
+$("#start").click(hideOthers);
+
+//Show the hidden content
+function showOthers() {
+  $(".clear").fadeIn(500);
+  $("#timerheading").removeClass("timerheading");
+}
+$(document).click(showOthers);
+
+// Fullscreen
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+}
+
+document.getElementById("fullscreen").addEventListener("click", toggleFullScreen);
