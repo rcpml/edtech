6ff629d12f1cb3d3c861162758c493a3c920d1d5
Recep Emül <recepemul@gmail.com>
2018-08-31 10:11:34 +0300

    Emoji movies added

--- GuesstheMovie/guessthemovie.js
+++ GuesstheMovie/guessthemovie.js
@@ -7,10 +7,6 @@ var emojiMovies = [{
         answer: "Batman"
     },
     {
-        question: "	&#x1F471;&#x1F3FB; &#x1F471;&#x1F3FC; &#x1F3C3;&#x1F3FB; &#x1F48D; &#x2197; &#x1F30B;",
-        answer:"The Lord of the Rings",
-    },
-    {
         question: "&#x1F466; &#x1F453; &#x26A1; &#x1F387; &#x1F989;",
         answer:"Harry Potter",
     },
@@ -41,6 +37,58 @@ var emojiMovies = [{
     {
         question: "&#x1F466;&#x1F3FB; &#x1F407; &#x0030;&#xFE0F;&#x20E3; &#x0031&#xFE0F;&#x20E3; &#x0030;&#xFE0F;&#x20E3; &#x0031&#xFE0F;&#x20E3; &#x1F576; &#x1F4DE; &#x1F5A5;",
         answer:"Matrix",
+    },
+    {
+        question: "&#x1F471;&#x1F3FB; &#x1F471;&#x1F3FC; &#x2694; &#x1F3F9; &#x1F48D; &#x1F30B;",
+        answer:"The Lord of the Rings",
+    },
+    {
+        question: "&#x1F47D; &#x1F471;&#x1F3FB; &#x1F6B2; &#x1F315;	",
+        answer:"E.T. the Extra-Terrestrial",
+    },
+    {
+        question: "&#x1F356; &#x1F357; &#x1F3B2;",
+        answer:"Hunger Games",
+    },
+    {
+        question: "&#x1F313; &#x1F491; &#x1F98A;",
+        answer:"Twilight",
+    },
+    {
+        question: "&#x1F466; &#x1F474; &#x1F697; &#x1F320; &#x23F1;",
+        answer:"Back to the Future",
+    },
+    {
+        question: "&#x1F63A; &#x1F462; &#x2694;	",
+        answer:"Puss in Boots",
+    },
+    {
+        question: "&#x1F935; &#x1F45E; &#x1F378; &#x1F1EC;&#x1F1E7;",
+        answer:"James Bond",
+    },
+    {
+        question: "&#x1F981; &#x1F451; &#x1F333;",
+        answer:"Lion King",
+    },
+    {
+        question: "&#x1F50E; &#x1F420; &#x1F41F; &#x1F30A; &#x1F422;",
+        answer:"Finding Nemo",
+    },
+    {
+        question: "&#x1F466; &#x1F3D4; &#x1F48E; &#x1F409; &#x1F48D;",
+        answer:"The Hobbit",
+    },
+    {
+        question: "&#x1F680; &#x1F647; &#x1F316; &#x1F331; &#x1F6F0; &#x1F64C;",
+        answer:"The Martian",
+    },
+    {
+        question: "&#x1F63E; &#x1F469; &#x1F94B; &#x1F938;",
+        answer:"Catwoman",
+    },
+    {
+        question: "&#x1F1FA;&#x1F1F8; &#x1F6E1; &#x1F44A;",
+        answer:"Captain America",
     }
     ];
 
@@ -81,3 +129,6 @@ function showSnack() {
 }
 document.getElementById('showsnack').addEventListener('click', showSnack);
 
+
+// //Delete after the insertion of new emojis
+// document.getElementById("new").innerHTML = emojiMovies[(emojiMovies.length-1)].question;
